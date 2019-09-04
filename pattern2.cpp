5053c58fdf6add6e0ab53794a2ea3a131567b7a8
sagar1612 <54577301+sagar1612@users.noreply.github.com>
2019-09-04 09:13:52 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<n;i++)
+    {
+    	for(int j=1;j<=15;j++)
+    	{
+    		cout<<j<<" ";
+            cout<<endl;
+    	}
+    	cout<<endl;
+    }
+
+	return 0;
+}
